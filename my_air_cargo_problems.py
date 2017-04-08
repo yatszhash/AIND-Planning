8bdc981b6752d3fcc0235e8030eee494422e8150
Sushi Yatsuhashi <sushi.yatsuhashi@gamil.com>
2017-04-08 11:38:15 +0900

    add result on my_air_cargo_problems

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -121,7 +121,6 @@ class AirCargoProblem(Problem):
         :return: list of Action objects
         """
         kb = PropKB()
-        sentenct = decode_state(state, self.state_map).pos
 
         kb.tell(decode_state(state, self.state_map).pos_sentence())
         possible_actions = [action for action in self.actions_list if action.check_precond(kb, action.args)]
@@ -139,8 +138,20 @@ class AirCargoProblem(Problem):
         :param action: Action applied
         :return: resulting state after action
         """
-        # TODO implement
         new_state = FluentState([], [])
+        old_state = decode_state(state, self.state_map)
+        for fluent in old_state.pos:
+            if fluent not in action.effect_rem:
+                new_state.pos.append(fluent)
+        for fluent in action.effect_add:
+            if fluent not in new_state.pos:
+                new_state.pos.append(fluent)
+        for fluent in old_state.neg:
+            if fluent not in action.effect_add:
+                new_state.neg.append(fluent)
+        for fluent in action.effect_rem:
+            if fluent not in new_state.neg:
+                new_state.neg.append(fluent)
         return encode_state(new_state, self.state_map)
 
     def goal_test(self, state: str) -> bool:
